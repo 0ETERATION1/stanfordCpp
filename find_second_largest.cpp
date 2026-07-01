54f946f912c38e8b438337bbb5bc5bb5eef7f7fc
Alex Terskin <terskinalex@gmail.com>
2026-06-30 23:26:03 -0400

    jun30th while mexico world cup game

---
+++ find_second_largest.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+/*
+ * For a slightly more interesting challenge, write a program that finds both the
+ * largest and the second-largest number in a list, prior to the entry of a sentinel.
+ * Once again using 0 as the sentinel, a sample run of this program might look like
+ *
+ */
+
+int main()
+{
+    std::cout << "This program finds the largest integer in a list." << std::endl;
+    std::cout << "Enter 0 to signal the end of the list" << std::endl;
+
+    const int sentinal = 0;
+    int largest = -9999;
+    int second_largest = -9999;
+    int user_input = -1;
+
+    // [223, 251, 317, 1, 6]
+    // u = 251
+    // l = 223
+    // sl = 223
+
+    std::cout << " ? ";
+    std::cin >> user_input;
+    largest = user_input;
+    second_largest = user_input;
+    std::cout << "outisde" << std::endl;
+
+    while (user_input != sentinal)
+    {
+
+        std::cout << " ? ";
+        std::cin >> user_input;
+        if (user_input > largest)
+        {
+            std::cout << "HELLO " << std::endl;
+            second_largest = largest;
+        }
+
+        largest = std::max(largest, user_input);
+    }
+
+    std::cout << "The largest value was: " << largest << std::endl;
+    std::cout << "The second largest value was: " << second_largest << std::endl;
+
+    return 0;
+}
