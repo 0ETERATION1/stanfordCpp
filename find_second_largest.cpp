251a566eee42f4b673835fd070b152e30b3a5c53
Alex Terskin <terskinalex@gmail.com>
2026-07-03 13:18:49 -0400

    fix second largest code

--- find_second_largest.cpp
+++ find_second_largest.cpp
@@ -1,49 +1,53 @@
 #include <iostream>
-#include <algorithm>
 /*
+ *
  * For a slightly more interesting challenge, write a program that finds both the
  * largest and the second-largest number in a list, prior to the entry of a sentinel.
  * Once again using 0 as the sentinel, a sample run of this program might look like
+ * this:
  *
  */
 
 int main()
 {
+    int sentinal = 0;
+    bool check = true;
+    int largest;
+    int secondLargest;
+    int user_input;
+    int count = 0;
+
     std::cout << "This program finds the largest integer in a list." << std::endl;
     std::cout << "Enter 0 to signal the end of the list" << std::endl;
 
-    const int sentinal = 0;
-    int largest = -9999;
-    int second_largest = -9999;
-    int user_input = -1;
-
-    // [223, 251, 317, 1, 6]
-    // u = 251
-    // l = 223
-    // sl = 223
-
-    std::cout << " ? ";
-    std::cin >> user_input;
-    largest = user_input;
-    second_largest = user_input;
-    std::cout << "outisde" << std::endl;
-
-    while (user_input != sentinal)
+    // 100, 50, 10
+    while (check)
     {
-
         std::cout << " ? ";
         std::cin >> user_input;
 
-        if (user_input > largest)
+        if (user_input == 0)
         {
-            second_largest = largest;
+            break;
         }
 
-        largest = std::max(largest, user_input);
+        if (count == 0)
+        {
+            largest = user_input;
+            secondLargest = user_input;
+            count++;
+        }
+        else if (user_input > largest)
+        {
+            secondLargest = largest;
+            largest = user_input;
+        }
+        else if (user_input > secondLargest)
+        {
+            secondLargest = user_input;
+        }
     }
-
     std::cout << "The largest value was: " << largest << std::endl;
-    std::cout << "The second largest value was: " << second_largest << std::endl;
-
+    std::cout << "The second largest value was: " << secondLargest << std::endl;
     return 0;
 }
